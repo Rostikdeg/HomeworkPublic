8bf0b08452ec0f6afc9bebf65712afa5c1ac2929
Rostik Dehtiarov <degtarovrostislav59@gmail.com>
2025-06-22 19:55:49 +0300

    home work

---
+++ 2.3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << /*був пропущен знак: " */"Is there a bug here?";/*був пропущен знак: ; */
+  return 0;
+}  
---
+++ 3.1-4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned long int age, YardArea, stars, JanuaryPrecipitation;
+const float pi = 3.14159;
+float a;
+
+int main(){
+    float a = pi;
+    cout << "float a = " << a;
+    return 0;
+}
